a54deede1a536975a26daa1abf96235e380d3138
Sergey UP <sergey10goals@gmail.com>
2024-03-29 19:13:57 +0200

    added some more custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,10 +28,22 @@ const config = {
         blue: {
           1: "#0E78F9",
         },
+        green: {
+          1: "#2cbfae",
+        },
         sky: {
           1: "#C9DDFF",
           2: "#ECF0FF",
           3: "#F5FCFF",
+        },
+        orange: {
+          1: "#FF742E",
+        },
+        purple: {
+          1: "#830EF9",
+        },
+        yellow: {
+          1: "#F9A90E",
         },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
