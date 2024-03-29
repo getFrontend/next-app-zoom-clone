498eeb1da582b78aa55dc046f31ae21879d0b701
Sergey UP <sergey10goals@gmail.com>
2024-03-29 19:15:47 +0200

    updated Navbar component

---
+++ components/MobileNav.tsx
@@ -0,0 +1,5 @@
+const MobileNav = () => {
+  return <div>MobileNav</div>;
+};
+
+export default MobileNav;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-full flex-between fixed z-50 bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          className="max-sm:w-16"
+          src="/icons/poom-logo.png"
+          alt="Poom logo"
+          width={200}
+          height={75}
+        />
+      </Link>
+
+      <div className="flex-between gap-5">
+        User
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
