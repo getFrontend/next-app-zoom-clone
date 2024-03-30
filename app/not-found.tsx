36aab24afc9a5550690e2e831ce57d5f2e4a3d4c
Sergey UP <sergey10goals@gmail.com>
2024-03-30 21:43:06 +0200

    Updated: improved the page not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,101 @@
+"use client";
+
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { useEffect } from "react";
+import { FiArrowRight } from "react-icons/fi";
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
 import Link from "next/link";
 
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C", "#2CBFAE"];
+
 export default function NotFound() {
+  const text =
+    "Sorry, but this page was not found! Please, return to the main page.".split(
+      " "
+    );
+
+  const color = useMotionValue(COLORS_TOP[0]);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
   return (
-    <div className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-green-1 font-bold text-7xl max-md:text-4xl">
-        Not found – 404!
-      </h1>
-      <div className="mt-16">
-        <Link href="/" className="text-white">
-          Go back to Home
-        </Link>
+    <motion.section
+      style={{
+        backgroundImage,
+      }}
+      className="relative grid min-h-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200"
+    >
+      <div className="relative z-10 flex flex-col items-center">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          drag
+          dragConstraints={{
+            top: -50,
+            left: -50,
+            right: 50,
+            bottom: 50,
+          }}
+        >
+          <h1 className="cursor-grab max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
+            POOM 404
+          </h1>
+        </motion.div>
+        <p className="my-16 max-w-2xl text-center text-base leading-relaxed md:text-xl md:leading-relaxed">
+          {text.map((el, i) => (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.25,
+                delay: i / 10,
+              }}
+              key={i}
+            >
+              {el}{" "}
+            </motion.span>
+          ))}
+        </p>
+        <Link href="/">
+        <motion.button
+          style={{
+            border,
+            boxShadow,
+          }}
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 1.5, duration: 0.5 }}
+          className="mt-16 group relative flex w-fit items-center gap-1.5 rounded-full bg-gray-950/10 px-10 py-6 text-gray-50"
+        >
+          Start from HOME
+          <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+        </motion.button>
+         </Link>
       </div>
-    </div>
+
+      <div className="absolute inset-0 z-0">
+        <Canvas>
+          <Stars radius={50} count={2500} factor={4} fade speed={2} />
+        </Canvas>
+      </div>
+    </motion.section>
   );
 }
