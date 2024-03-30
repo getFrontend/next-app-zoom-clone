24a85d89dea2bde82b3237ad55ab103bf9c1ef8f
Sergey UP <sergey10goals@gmail.com>
2024-03-31 00:25:15 +0200

    added MeetingTypeList component

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import HomeCard from "./HomeCard";
+
+const MeetingTypeList = () => {
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >(undefined);
+  const router = useRouter();
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        className="bg-blue-1"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link"
+        className="bg-green-1"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        className="bg-orange-1"
+        handleClick={() => router.push("/recordings")}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
