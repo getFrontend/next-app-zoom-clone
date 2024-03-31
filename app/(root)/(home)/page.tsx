94fade6309c012e8b2f7de7323ca9a3b7cbf8dff
Sergey UP <sergey10goals@gmail.com>
2024-03-31 13:00:10 +0300

    implemented background-gradient effect

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
-import MeetingTypeList from "@/components/MeetingTypeList";
+"use client";
+
+import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { Meteors } from "@/components/ui/meteors";
+import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
   const now = new Date();
@@ -16,12 +19,18 @@ const Home = () => {
     <section className="size-full flex flex-col gap-10 text-white">
       <div className="relative overflow-hidden h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between items-end max-md:px-5 max-md:py-8 p-6 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded-xl px-6 py-2 text-center text-base font-normal text-dark-1">
+          <BackgroundGradient className="glassmorphism max-w-[273px] rounded-[20px] px-6 py-2">
+          <h2 className="text-center text-base font-normal text-dark-1">
             Upcoming Meeting at: <span className="font-semibold">12:00</span>{" "}
           </h2>
+          </BackgroundGradient>
           <div className="flex flex-col gap-2">
-            <h3 className="text-center text-4xl font-extrabold lg:text-7xl">{time}</h3>
-            <p className="text-center text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+            <h3 className="text-center text-4xl font-extrabold lg:text-7xl">
+              {time}
+            </h3>
+            <p className="text-center text-lg font-medium text-sky-1 lg:text-2xl">
+              {date}
+            </p>
           </div>
         </div>
         <Meteors number={33} />
