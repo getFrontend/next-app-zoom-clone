980c6d3185ac376bf6e86434c902ccb4cd69fd7a
Sergey UP <sergey10goals@gmail.com>
2024-04-01 23:48:38 +0300

    implemented EndCallButton component to end the call by owner of meeting room

---
+++ components/EndCallButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { Button } from "./ui/button";
+
+const EndCallButton = () => {
+  const router = useRouter();
+  const call = useCall();
+
+  if (!call)
+    throw new Error(
+      "useStreamCall must be used within a StreamCall component!"
+    );
+
+  // https://getstream.io/video/docs/react/guides/call-and-participant-state/#participant-state-3
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
+
+  const isMeetingOwner =
+    localParticipant &&
+    call.state.createdBy &&
+    localParticipant.userId === call.state.createdBy.id;
+
+  if (!isMeetingOwner) return null;
+
+  const endCall = async () => {
+    await call.endCall();
+    router.push("/");
+  };
+
+  return (
+    <Button onClick={endCall} className="bg-orange-1 hover:bg-red-500">
+      End call for everyone
+    </Button>
+  );
+};
+
+export default EndCallButton;
