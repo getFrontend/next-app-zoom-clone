5d7dc66490ee0adf8c58498cdd00c0ae51d75c5b
Sergey UP <sergey10goals@gmail.com>
2024-04-02 10:31:10 +0300

    added avatars samples

--- constants/index.ts
+++ constants/index.ts
@@ -29,6 +29,15 @@ export const sidebarLinks = [
 
 export const initialValues = {
   dateTime: new Date(),
-  description: '',
-  link: '',
+  description: "",
+  link: "",
 };
+
+export const avatarImages = [
+  "/images/avatar-1.jpg",
+  "/images/avatar-2.jpg",
+  "/images/avatar-3.jpg",
+  "/images/avatar-4.jpg",
+  "/images/avatar-5.jpg",
+  // "/images/avatar-6.jpg",
+];
