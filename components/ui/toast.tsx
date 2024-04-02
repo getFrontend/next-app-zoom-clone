8cb576c192c62e62c9887cdcc86e3593365a02d3
Sergey UP <sergey10goals@gmail.com>
2024-04-02 19:10:42 +0300

    fixed X-close button size

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -83,7 +83,7 @@ const ToastClose = React.forwardRef<
     toast-close=""
     {...props}
   >
-    <X className="h-4 w-4" />
+    <X className="h-6 w-6" />
   </ToastPrimitives.Close>
 ))
 ToastClose.displayName = ToastPrimitives.Close.displayName
